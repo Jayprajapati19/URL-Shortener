2e683030eac8879ba76e5881e66bfabfe19ee8ed
Jay Prajapati <jayp37679@gmail.com>
2026-01-01 11:53:18 +0530

    Implement MongoDB connection and URL shortener model

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,28 @@
 import express from "express"
-import { REFUSED } from "node:dns";
+import {nanoid} from "nanoid"
+import dotenv from "dotenv"
+dotenv.config();
+import filecabinet from "./src/config/mongo.config.js"
+
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-
-app.get("/api/create", (req, res) => {
+app.post("/api/create", (req, res) => {
+    const {url} = req.body
+   
     
+    res.send(nanoid(7));
 })
 
 
 app.listen(5000, () => {
+    filecabinet();
     console.log("Server is Runnig on port http://localhost:5000");
     
 })
+
 
 // GET - rediration
 // POST - create short URL
