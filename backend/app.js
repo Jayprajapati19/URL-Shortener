f1b9dd4e091314314ca0761b29d1e02c1daad607
Jay Prajapati <jayp37679@gmail.com>
2026-01-01 12:10:47 +0530

    Refactor app.js: reorganize imports and enhance URL creation logic

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
+import dotenv from "dotenv"
 import express from "express"
 import {nanoid} from "nanoid"
-import dotenv from "dotenv"
 dotenv.config();
 import filecabinet from "./src/config/mongo.config.js"
-
+import urlSchema from "./src/models/shorturl.model.js"
 
 const app = express();
 app.use(express.json());
@@ -11,9 +11,24 @@ app.use(express.urlencoded({extended: true}));
 
 app.post("/api/create", (req, res) => {
     const {url} = req.body
-   
-    
+    const shortUrl = nanoid(7)
+    const newUrl = new urlSchema({
+        full_url: url,
+        short_url: shortUrl
+    })
+    newUrl.save();
+
     res.send(nanoid(7));
+})
+
+app.get("/:id", async (req, res) => {
+    const {id} = req.params;
+    const url = await urlSchema.findOne({short_url: id});
+    if (url) {
+         res.redirect(url.full_url);
+    } else {
+         res.status(404).json("URL not found");
+    }
 })
 
 
@@ -25,7 +40,6 @@ app.listen(5000, () => {
 
 
 // GET - rediration
-// POST - create short URL
 
 
 
