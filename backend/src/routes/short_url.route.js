af52a8f5f27ec14640ff5a86ac312859e74925aa
Jay Prajapati <jayp37679@gmail.com>
2026-01-02 14:20:44 +0530

    remove

--- backend/src/routes/short_url.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { createShortUrl } from "../controller/short_url.controller.js";
-const router = express.Router();
-
-router.post("/create", createShortUrl)
-
-
-export default router;
